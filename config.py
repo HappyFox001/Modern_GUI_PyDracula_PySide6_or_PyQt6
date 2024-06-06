57af323087076adc56d1046ae215a8bad8c9b698
Zhang Qian <qianzhang.happyfox@gmail.com>
2024-06-06 19:40:27 +0800

    Describe your changes

---
+++ config.py
@@ -0,0 +1,38 @@
+YOUR_INFURA_PROJECT_ID = 'c9c036f6af194bb0aebc16d4bc5bbf88'
+infura_url = f"https://mainnet.infura.io/v3/{YOUR_INFURA_PROJECT_ID}"
+wallet_address = "0xbdfa4f4492dd7b7cf211209c4791af8d52bf5c50"
+CoinGecko = "https://api.coingecko.com/api/v3/simple/price"
+Token_Name = {
+    'ETH': 'ethereum',
+    'USDT': 'tether',
+    'USDC': 'usd-coin',
+    'DAI': 'dai',
+    'BNB': 'binancecoin',
+    'UNI': 'uniswap',
+    'WBTC': 'wrapped-bitcoin',
+    'LINK': 'chainlink',
+    'AAVE': 'aave',
+    'YFI': 'yearn-finance',
+    'COMP': 'compound',
+    'SNX': 'synthetix',
+    'SUSHI': 'sushiswap',
+    'MKR': 'maker',
+    'MATIC': 'matic-network',
+}
+Token_Address = {
+    'USDT': '0xdac17f958d2ee523a2206206994597c13d831ec7',
+    'USDC': '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48',
+    'DAI': '0x6b175474e89094c44da98b954eedeac495271d0f',
+    'BNB': '0xb8c77482e45f1f44de1745f52c74426c631bdd52',
+    'UNI': '0x1f9840a85d5af5bf1d1762f925bdaddc4201f984',
+    'WBTC': '0x2260fac5e5542a773aa44fbcfedf7c193bc2c599',
+    'LINK': '0x514910771af9ca656af840dff83e8264ecf986ca',
+    'AAVE': '0x7fc66500c84a76ad7e9c93437bfc5ac33e2ddae9',
+    'YFI': '0x0bc529c00C6401aEF6D220BE8C6Ea1667F6Ad93e',
+    'COMP': '0xc00e94cb662c3520282e6f5717214004a7f26888',
+    'SNX': '0xc011a72400e58ecd99ee497cf89e3775d4bd732f',
+    'SUSHI': '0x6b3595068778dd592e39a122f4f5a5cf09c90fe2',
+    'MKR': '0x9f8f72aa9304c8b593d555f12ef6589cc3a579a2',
+    'MATIC': '0x7d1afa7b718fb893db30a3abc0cfc608aacfebb0'
+}
+Wallet_Map={}
